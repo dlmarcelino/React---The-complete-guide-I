bb688854b3c21c15bff25d53636b1298e19ce2b6
dlmarcelino <duartemarcelino@hotmail.com>
2022-02-19 13:48:33 +0000

    first commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,15 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  const expensesList = props.items.map((element) => (
+    <ExpenseItem
+      title={element.title}
+      amount={element.amount}
+      date={element.date}
+    />
+  ));
+  return <Card className="expenses">{expensesList}</Card>;
+}
+export default Expenses;
